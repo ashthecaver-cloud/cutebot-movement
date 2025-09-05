97f54f6fa46a9a00318a17a9407f20a7b2e21f51
John Walter <ashthecaver@gmail.com>
2025-09-05 15:27:42 -0400

    Took out forever command

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 basic.showString("\"IoT4Ag!\"")
-basic.forever(function () {
-    cuteBot.motors(53, 53)
-    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
-    cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
-    cuteBot.stopcar()
-})
+cuteBot.motors(53, 53)
+cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
+cuteBot.moveTime(cuteBot.Direction.forward, 50, 2)
+cuteBot.stopcar()
